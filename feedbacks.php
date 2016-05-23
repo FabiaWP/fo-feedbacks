a376a7a36d78a34033adf057fd49a805f05e7054
Fabia Omezzoli <fabia.omezzoli@gmail.com>
2016-05-23 20:10:12 +0200

    Formatted code

--- feedbacks.php
+++ feedbacks.php
@@ -2,13 +2,13 @@
 /*
 Plugin Name: FeedBacks Fabia OOP
 Plugin URI:
+*/
 
- */
-define('FO_FEEDBACKS_PATH', dirname(__FILE__));
-define('FO_FEEDBACKS_FOLDER', basename(FO_FEEDBACKS_PATH));
-define('FO_FEEDBACKS_URL', plugins_url() . '/' . FO_FEEDBACKS_FOLDER);
+define( 'FO_FEEDBACKS_PATH', dirname( __FILE__ ) );
+define( 'FO_FEEDBACKS_FOLDER', basename( FO_FEEDBACKS_PATH ) );
+define( 'FO_FEEDBACKS_URL', plugins_url() . '/' . FO_FEEDBACKS_FOLDER );
 
-require_once __DIR__.'/class-fo-feedbacks2.php';
+require_once __DIR__ . '/class-fo-feedbacks2.php';
 
 new FO_FEEDBACKS();
 
